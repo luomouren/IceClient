31ea2b54fb5598971d359e2e908fc122ecba1ee8
Amy <luomouren@foxmail.com>
2017-01-12 09:54:34 +0800

    ice grid 配置1

---
+++ src/main/java/com/weisi/Client/frame/init/MyClient2.java
@@ -0,0 +1,42 @@
+package com.weisi.Client.frame.init;
+
+import com.weisi.Server.serverIce.MyServicePrx;
+import com.weisi.Server.serverIce.MyServicePrxHelper;
+
+public class MyClient2 {
+    /**
+     * 简单启动客户端
+     * @param args
+     */
+    public static void main(String[] args) {
+      int status=0;
+      Ice.Communicator ic =null;
+      try {
+        //初始化通信器
+        String reg ="--Ice.Default.Locator=IceGrid/Locator:tcp -h localhost -p 4061";
+        String[] parms = new String[]{reg};
+        ic=Ice.Util.initialize(parms);
+        //传入远程服务单元的名称、网络协议、Ip以及端口，构造一个Proxy对象
+        Ice.ObjectPrx base = ic.stringToProxy("MyService");
+        //通过checkedCast向下转型，获取MyService接口的远程，并同时监测根据传入的名称，
+        //获取服务单元是否OnlineBook的代理接口，如果不是则返回null对象
+        MyServicePrx prxy = MyServicePrxHelper.uncheckedCast(base);
+        if(prxy==null){
+          throw new Error("Invalid proxy");
+        }
+        //调用服务方法
+        String rt=prxy.hellow();
+        System.out.println("prxy.hellow()===="+rt);
+        
+      } catch (Exception e) {
+        e.printStackTrace();
+        status=1;
+      }finally{
+        if(ic!=null){
+          ic.destroy();
+        }
+      }
+      System.exit(status);
+      
+    }
+}
