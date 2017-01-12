b09a33620b4e4d5bf9ff77f06807b8769f02605b
Amy <luomouren@foxmail.com>
2017-01-12 19:11:45 +0800

    IceBox.Server测试成功

--- src/main/java/com/weisi/Client/frame/init/SwitchClient.java
+++ src/main/java/com/weisi/Client/frame/init/SwitchClient.java
@@ -23,7 +23,7 @@ public class SwitchClient {
         //初始化通信器
         ic=Ice.Util.initialize(args);
         //传入远程服务单元的名称、网络协议、Ip以及端口，构造一个Proxy对象
-        Ice.ObjectPrx base = ic.stringToProxy("Switch:default -p 5010");
+        Ice.ObjectPrx base = ic.stringToProxy("SwitchServer:default -p 20000");
         //通过checkedCast向下转型，获取MyService接口的远程，并同时监测根据传入的名称，
         //获取服务单元是否OnlineBook的代理接口，如果不是则返回null对象
         ISwitchPrx switchPushPrx = ISwitchPrxHelper.checkedCast(base);
